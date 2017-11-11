38e7874035fb1ee95f3f01e7e42fa9bb3e0a8d87
Iliana <iliana.pinto04@gmail.com>
2017-11-11 12:10:55 -0600

    menu

--- main.cpp
+++ main.cpp
@@ -6,9 +6,41 @@ void limpiar(char**,int);
 void imprimir(char**,int);
 
 int main(int argc, char const *argv[]) {
-    do {
-
-    } while(true);
+    char** matriz;
+    int size;
+    cout<<"-----BIENVENIDO-----"<<endl;
+        int resp, resp2;
+        cout<<"Elija el tamaño"<<endl
+        <<" 1. 4X4"<<endl
+        <<" 2. 3X3"<<endl
+        <<" 3. 2X2"<<endl;
+        cin>>resp;
+        while (resp < 1 || resp > 3) {
+            cout<<"Opcion invalida, ingrese nuevamente"<<endl;
+            cin>>resp;
+        }
+        cout<<endl;
+        cout<<"Elija con quien desea jugar"<<endl
+        <<" 1. Multiplayer"<<endl
+        <<" 2. Computadora"<<endl;
+        cin>>resp2;
+        while (resp2 < 1 || resp2 > 2) {
+            cout<<"Opcion invalida, ingrese nuevamente"<<endl;
+            cin>>resp2;
+        }
+        cout<<endl;
+        if (resp == 1) {
+            matriz = crear(4);
+            size = 4;
+        }else if(resp == 2){
+            matriz = crear(3);
+            size = 3;
+        }else{
+            matriz = crear(2);
+            size = 2;
+        }
+        
+    limpiar(matriz,size);
     return 0;
 }
 
